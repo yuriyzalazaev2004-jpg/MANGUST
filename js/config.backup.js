8b14c761d5cf4c4209024aa3fe589d44075a1c0d
K1NG4 <yuriyzalazaev2004@gmail.com>
2026-09-25 20:22:14 +0400

    Delete js directory

--- js/config.backup.js
+++
@@ -1,8 +0,0 @@
-// Публичная конфигурация сайта.
-// ВАЖНО: сюда можно вставлять только Publishable key (sb_publishable_...).
-// НИКОГДА не вставляй сюда Secret key / service_role key.
-window.APP_CONFIG = {
-  SUPABASE_URL: 'https://YOUR_PROJECT_REF.supabase.co',
-  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_YOUR_KEY',
-  DONATION_URL: 'https://www.donationalerts.com/r/YOUR_CHANNEL'
-};
